65874cb106a8c72b983df937047d0ab6baa3b728
Duncan Clarke <duncanapclarke@outlook.com>
2025-10-27 17:29:47 +0000

    Testing build...

---
+++ projects/maven/rest-service-dctest/src/test/java/com/example/restservice/StepDefinitions.java
@@ -0,0 +1,34 @@
+package hellocucumber;
+
+import io.cucumber.java.en.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+class IsItFriday {
+    static String isItFriday(String today) {
+        // return null;
+        // return "Nope";
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
+*/
+
+public class StepDefinitions {
+
+    //private String today;
+    ///private String actualAnswer;
+
+    @Given("an example scenario")
+    public void anExampleScenario() {
+    }
+
+    @When("all step definitions are implemented")
+    public void allStepDefinitionsAreImplemented() {
+    }
+
+    @Then("the scenario passes")
+    public void theScenarioPasses() {
+    }
+
+}
